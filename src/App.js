f3e79f0f9525fbe8798f9dce211f9b3cc36af963
Chiến <caochientp1@gmail.com>
2020-04-09 01:19:58 +0700

    set cover

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 // Import components
 import Navbar from './components/Navbar';
+import Cover from './components/Cover';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Cover />
     </div>
   );
 }
